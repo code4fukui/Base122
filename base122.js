f90b8fa433fbe290a5459b5fd497411e6b3590e1
Taisuke Fukuno <fukuno@jig.jp>
2023-01-11 13:45:59 +0900

    extract only encode / decode

--- base122.js
+++ base122.js
@@ -1,23 +1,16 @@
-// Provides functions for encoding/decoding data to and from base-122.
-
-let fs = require('fs')
-, readline = require('readline')
-;
 
 const kString = 0
-, kUint8Array = 1
-, kDefaultMimeType = "image/jpeg"
-, kDebug = false
-, kIllegals = [
+const kUint8Array = 1
+const kDebug = false
+const kIllegals = [
     0 // null
     , 10 // newline
     , 13 // carriage return
     , 34 // double quote
     , 38 // ampersand
     , 92 // backslash
-]
-, kShortened = 0b111 // Uses the illegal index to signify the last two-byte char encodes <= 7 bits.
-;
+];
+const kShortened = 0b111 // Uses the illegal index to signify the last two-byte char encodes <= 7 bits.
 
 /**
  * Encodes raw data into base-122.
@@ -91,82 +84,6 @@ function encode(rawData) {
 }
 
 /**
- * Re-encodes an HTML or text file with base-64 data to one with base-122 data.
- * @param {String} inpath - The filepath of the input file.
- * @param {String} outpath - The filepath of the output file.
- * @param {Object} options
- * @param {Boolean} options.html - Parse the input file as HTML and re-encode base64 data URIs.
- * @param {Boolean} options.addDecoder - If HTML, insert the minified decoder before </body>.
- * @param {Function} callback - Called upon completion.
- */
-function encodeFile(inpath, outpath, options, callback) {
-    let inStream = fs.createReadStream(inpath, {encoding: 'utf8'});
-    let outStream = fs.createWriteStream(outpath, {defaultEncoding: 'utf8'});
-    let decoderScript = options.addDecoder ? fs.readFileSync('decode.min.js') : '';
-
-    outStream.on('error', () => { throw 'Error writing to ' + outpath; });
-    inStream.on('error', () => { throw 'Error reading from ' + inpath; });
-
-    if (!options.html) {
-        // This is a plain base-64 encoded file.
-        let fileContents = "";
-        inStream.on('data', (chunk) => { fileContents += chunk; });
-        inStream.on('end', () => {
-            let encoded = encodeFromBase64(fileContents);
-            let encodedStr = String.fromCharCode(...encoded);
-            outStream.end(encodedStr, 'binary', callback);
-        });
-        return;
-    }
-
-    let rl = readline.createInterface({ input: inStream });
-    rl.on('line', (line) => {
-        let regexp = /src=[\"\']data:(.*);base64,(.*?)[\"\']/ig;
-        let bodyRegExp = /<\/body>/i;
-        let results;
-        let prevIndex = 0;
-        while ((results = regexp.exec(line)) !== null) {
-            outStream.write(line.substring(prevIndex, results.index));
-            let mimetype = results[1];
-            let encoded = encodeFromBase64(results[2]);
-            let encodedStr = String.fromCharCode(...encoded);
-            outStream.write('data-b122="');
-            outStream.write(encodedStr, 'binary');
-            outStream.write('"');
-            if (mimetype != kDefaultMimeType) outStream.write(' data-b122m="' + mimetype + '"');
-            prevIndex = regexp.lastIndex;
-        }
-        if (options.addDecoder) {
-            // Check for </body> to insert decoder.
-            if ((results = bodyRegExp.exec(line)) != null) {
-                // </body> cannot be valid if it's before any data URI.
-                if (results.index >= prevIndex) {
-                    outStream.write(line.substring(0, results.index) + '<script>' + decoderScript
-                        + '</script>');
-                    prevIndex = results.index;
-                }
-            }
-        }
-        outStream.write(line.substring(prevIndex) + "\n");
-    });
-
-    rl.on('close', () => {
-        outStream.end('', callback);
-    });
-}
-
-/**
- * Re-encodes a base-64 encoded string into base-122.
- * @param {String} base64String - A base-64 encoded string.
- * @returns {Array} - The base-122 encoded data.
- */
-function encodeFromBase64(base64String) {
-    // "binary" encoding encodes each byte in a separate character.
-    let strData = Buffer.from(base64String, 'base64').toString('binary');
-    return encode(strData);
-}
-
-/**
  * Decodes base-122 encoded data back to the original data.
  * @param {Uint8Array|Buffer|String} rawData - The data to be decoded. This can be a Uint8Array
  * or Buffer with raw data bytes or a string of bytes (i.e. the type of argument to btoa())
@@ -175,7 +92,6 @@ function encodeFromBase64(base64String) {
 function decode(base122Data) {
     let strData = typeof(base122Data) == 'string' ? base122Data : utf8DataToString(base122Data)
     , decoded = []
-    , decodedIndex = 0
     , curByte = 0
     , bitOfByte = 0
     ;
@@ -213,35 +129,4 @@ function decode(base122Data) {
     return decoded;
 }
 
-
-/**
- * Converts a sequence of UTF-8 bytes to a string.
- * @param {Uint8Array|Buffer} data - The UTF-8 data.
- * @returns {String} A string with each character representing a code point.
- */
-function utf8DataToString(data) {
-    return Buffer.from(data).toString('utf-8');
-}
-
-// For debugging.
-function debugLog() {
-    if (kDebug) console.log(...arguments);
-}
-
-// For debugging.
-function print7Bits(num) {
-    return "0000000".substring(num.toString(2).length) + num.toString(2);
-}
-
-// For debugging.
-function print8Bits(num) {
-    return "00000000".substring(num.toString(2).length) + num.toString(2);
-}
-
-module.exports = {
-    encode: encode,
-    decode: decode,
-    encodeFromBase64: encodeFromBase64,
-    encodeFile: encodeFile,
-    utf8DataToString: utf8DataToString
-};
+export const Base128 = { encode, decode };
